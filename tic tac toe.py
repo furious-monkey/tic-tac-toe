e90c8aded0669d5110db151e633ea0033bd03644
furious-monkey <daniel.j.hahn1105@gmail.com>
2020-02-27 23:21:20 +0530

    Update tic tac toe.py

--- tic tac toe.py
+++ tic tac toe.py
@@ -7,13 +7,14 @@ current_player = "X"
 
 def play_game():
   print("\n    X - O     G A M E\n")
+	print("Note : Use the numpad to enter X or O in respective fields.\n")
   display_board()
   while game_still_going:
     handle_turn(current_player)
     check_if_game_over()
     flip_player()
   if winner == "X" or winner == "O":
-    print("PLAYER",winner," IS THE WINNER !!.")
+    print("PLAYER",winner,"IS THE WINNER !!.")
   elif winner == None:
     print("Tie.")
   print("\nT H A N K S     F O R     P L A Y I N G     ! ! ! ! !")
